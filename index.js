9e3fcacb581a61432323a588b05bcf21c09fcf2f
Collins <mutaic84@gmail.com>
2022-04-18 12:05:04 +0300

    lab solution

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  for(let i=0; i<array.length; i++){
+    const compliment = target - array[i]
+    for(let j=i+1; j<array.length; j++){
+      if (array[j] === compliment) return true
+    }
+  }
+  return false
 }
 
 /* 
   Write the Big O time complexity of your function here
+  O(n^2)
 */
 
 /* 
   Add your pseudocode here
+  iterate thro each number in array
+    for current num identify compliment adding up to target (target - current number)
+    loop remaining elements of array
+      if any num is compliment
+        return true
+  return fals if no num
 */
 
 /*
